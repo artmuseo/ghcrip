9f79c23789467c2e426e85872c38b698df4ac1e1
Liam Monninger <l.mak.monninger@gmail.com>
2022-10-03 15:50:38 -0700

    Escape.

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ cat > $GHCR_DIR/ghcrip <<EOT
 set -e
 
 export UUID=$(uuidgen)
-export REPO="$1"
-export EXECUTABLE="$2"
-export COMMIT="${3:-main}"
+export REPO="\$1"
+export EXECUTABLE="\$2"
+export COMMIT="\${3:-main}"
 
 cat > ./.ghcrip.gitignore << EOF
 *.bstate
@@ -26,18 +26,18 @@ cat > ./.ghcrip.gitignore << EOF
 *.ghcrip
 EOF
 
-export TEMP_DIR="$(pwd)/.ghcrip/$(echo ${REPO} | awk -F// '{print $NF}')"
-if test -d $TEMP_DIR; then
-    echo "Using cached repo." >> ${TEMP_DIR}/script.log 2>&1
+export TEMP_DIR="$(pwd)/.ghcrip/$(echo \${REPO} | awk -F// '{print $NF}')"
+if test -d \$TEMP_DIR; then
+    echo "Using cached repo." >> \${TEMP_DIR}/script.log 2>&1
 else
-    mkdir -p $TEMP_DIR
-    git clone $REPO $TEMP_DIR  >> ${TEMP_DIR}/script.log 2>&1
+    mkdir -p \$TEMP_DIR
+    git clone \$REPO \$TEMP_DIR  >> \${TEMP_DIR}/script.log 2>&1
 fi
 
-echo "Checking out..." >> ${TEMP_DIR}/script.log
-git -C $TEMP_DIR checkout ${COMMIT} >> ${TEMP_DIR}/script.log 2>&1
-git -C $TEMP_DIR pull >> ${TEMP_DIR}/script.log 2>&1
-/bin/bash $TEMP_DIR/$EXECUTABLE "$@"
+echo "Checking out..." >> \${TEMP_DIR}/script.log
+git -C \$TEMP_DIR checkout \${COMMIT} >> \${TEMP_DIR}/script.log 2>&1
+git -C \$TEMP_DIR pull >> \${TEMP_DIR}/script.log 2>&1
+/bin/bash \$TEMP_DIR/\$EXECUTABLE "\$@"
 EOT
 
 chmod -R 755 $GHCR_DIR
